9fff5c7e67e8cb0cd616839e747fe5fa75dbb4af
wangxianli <wangxianli@uniontech.com>
2022-02-10 09:59:47 +0800

    增加运行指令说明 双向链表的案例

--- mygocode/golang-design-pattern-master/00_simple_factory/simple_test.go
+++ mygocode/golang-design-pattern-master/00_simple_factory/simple_test.go
@@ -12,7 +12,7 @@ func TestType1(t *testing.T) {
 }
 
 func TestType2(t *testing.T) {
-	api := NewAPI(2)
+	var api = NewAPI(2)
 	s := api.Say("Tom")
 	if s != "Hello, Tom" {
 		t.Fatal("Type2 test fail")
